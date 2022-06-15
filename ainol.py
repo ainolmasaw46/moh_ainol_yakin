e1244be102553f6d619f4e1dc09b99785a5b486e
ainolmasaw46 <107546307+ainolmasaw46@users.noreply.github.com>
2022-06-15 17:19:56 +0700

    Add files via upload

---
+++ ainol.py
@@ -0,0 +1,33 @@
+print('kalkulator_sederhana')
+print('penjumlahan {-}')
+print('pengurangan {-}')
+print('perkalian {*}')
+print('pembagian {/}')
+
+operasi=input('(1|2|3|4|)')
+bilangan_1 = eval(input('masukkan bilangan pertama:'))
+bilangan_2 = eval(input('masukkan bilangan kedua:'))
+
+if operasi == '1' :
+    hasil = bilangan_1 + bilangan_2
+    print(f'masukkan hasil operasi {bilangan_1 } + {bilangan_2} = {hasil}')
+elif operasi =='2' :
+    hasil = bilangan_1 - bilangan_2
+    print(f'masukkan hasil operasi {bilangan_1 } - {bilangan_2} = {hasil}')
+elif operasi == '3' :
+    hasil = bilangan_1 * bilangan_2
+    print(f'masukkan hasil operasi {bilangan_1 } * {bilangan_2} = {hasil}')
+elif operasi == '4' :
+    hasil = bilangan_1 / bilangan_2
+    print(f'masukkan hasil operasi {bilangan_1 } / {bilangan_2} = {hasil}')
+else :
+    print('coba lagi')
+    
+class jawaban :
+    def __init__(self,jawaban) :
+        self,jawaban = jawaban
+
+class jawaban :
+    pass
+    ('jawaban benar sekali')
+    print('jawaban benar sekali')
